cd4942074fc09b0c8b438cb41ac513d1b02a1508
Sean Seefried <sean.seefried@gmail.com>
2019-08-01 11:38:41 +1000

    Add initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] Paddle paddle1;
+
+    // State
+    Vector2 paddleToBallVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = paddle1.transform.position;
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = transform.position;
+        paddlePos.x = Mathf.Clamp(xPositionInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
