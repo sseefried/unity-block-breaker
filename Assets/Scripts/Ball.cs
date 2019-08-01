b3a10fa7dabf110e5291df1d652afbdc76104759
Sean Seefried <sean.seefried@gmail.com>
2019-08-01 13:17:43 +1000

    Add block breaking

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
     // Configuration parameters
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
     [SerializeField] Paddle paddle1;
+    bool hasStarted = false;
+
 
     // State
     Vector2 paddleToBallVector;
@@ -18,6 +23,24 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = paddle1.transform.position;
         transform.position = paddlePos + paddleToBallVector;
